ca3718ce7a5d2985c1d8cdadbe7ac89fc051d9e9
Conrad Kramer <conrad@kramerapps.com>
2013-11-10 14:44:04 -0500

    More whitespace

--- Selection.c
+++ Selection.c
@@ -11,85 +11,85 @@ bool enabled = false;
 bool doneSelection = false;
 
 task selection() {
-  doneSelection = false;
+	doneSelection = false;
 
-  displayLCDCenteredString(0, "Select Zone");
-  displayLCDString(1, 0, "Hanging");
-  displayLCDString(1, 10, "Middle");
-  int buttons = 0;
-  while (true) {
-    if ((buttons & 0x1) && !(nLCDButtons & 0x1)) {
-      fieldZone = ZONE_HANGING;
-      break;
-    }
-    if ((buttons & 0x4) && !(nLCDButtons & 0x4)) {
-      fieldZone = ZONE_MIDDLE;
-      break;
-    }
+	displayLCDCenteredString(0, "Select Zone");
+	displayLCDString(1, 0, "Hanging");
+	displayLCDString(1, 10, "Middle");
+	int buttons = 0;
+	while (true) {
+		if ((buttons & 0x1) && !(nLCDButtons & 0x1)) {
+			fieldZone = ZONE_HANGING;
+			break;
+		}
+		if ((buttons & 0x4) && !(nLCDButtons & 0x4)) {
+			fieldZone = ZONE_MIDDLE;
+			break;
+		}
 
-    buttons = nLCDButtons;
-    wait1Msec(20);
-  }
+		buttons = nLCDButtons;
+		wait1Msec(20);
+	}
 
-  clearLCDLine(0);
-  clearLCDLine(1);
+	clearLCDLine(0);
+	clearLCDLine(1);
 
-  displayLCDCenteredString(0, "Select Color");
-  displayLCDString(1, 0, "Blue");
-  displayLCDString(1, 13, "Red");
-  buttons = 0;
-  while (true) {
-    if ((buttons & 0x1) && !(nLCDButtons & 0x1)) {
-      fieldColor = COLOR_BLUE;
-      break;
-    }
-    if ((buttons & 0x4) && !(nLCDButtons & 0x4)) {
-      fieldColor = COLOR_RED;
-      break;
-    }
+	displayLCDCenteredString(0, "Select Color");
+	displayLCDString(1, 0, "Blue");
+	displayLCDString(1, 13, "Red");
+	buttons = 0;
+	while (true) {
+		if ((buttons & 0x1) && !(nLCDButtons & 0x1)) {
+			fieldColor = COLOR_BLUE;
+			break;
+		}
+		if ((buttons & 0x4) && !(nLCDButtons & 0x4)) {
+			fieldColor = COLOR_RED;
+			break;
+		}
 
-    buttons = nLCDButtons;
-    wait1Msec(20);
-  }
+		buttons = nLCDButtons;
+		wait1Msec(20);
+	}
 
-  clearLCDLine(0);
-  clearLCDLine(1);
+	clearLCDLine(0);
+	clearLCDLine(1);
 
-  displayLCDCenteredString(0, "Enabled");
-  displayLCDString(1, 0, "Yes");
-  displayLCDString(1, 11, "No");
-  buttons = 0;
-  while (true) {
-    if ((buttons & 0x1) && !(nLCDButtons & 0x1)) {
-      enabled = true;
-      break;
-    }
-    if ((buttons & 0x4) && !(nLCDButtons & 0x4)) {
-      enabled = false;
-      break;
-    }
+	displayLCDCenteredString(0, "Enabled");
+	displayLCDString(1, 0, "Yes");
+	displayLCDString(1, 11, "No");
+	buttons = 0;
+	while (true) {
+		if ((buttons & 0x1) && !(nLCDButtons & 0x1)) {
+			enabled = true;
+			break;
+		}
+		if ((buttons & 0x4) && !(nLCDButtons & 0x4)) {
+			enabled = false;
+			break;
+		}
 
-    buttons = nLCDButtons;
-    wait1Msec(20);
-  }
+		buttons = nLCDButtons;
+		wait1Msec(20);
+	}
 
-  clearLCDLine(0);
-  clearLCDLine(1);
+	clearLCDLine(0);
+	clearLCDLine(1);
 
-  doneSelection = true;
+	doneSelection = true;
 }
 
 void SelectAutonomousRoutine() {
-  if (!bIfiRobotDisabled) return;
+	if (!bIfiRobotDisabled) return;
 
-  bLCDBacklight = true;
-  StartTask(selection);
+	bLCDBacklight = true;
+	StartTask(selection);
 
-  while (!doneSelection) {
-    if (!bIfiRobotDisabled) doneSelection = true;
-    wait1Msec(25);
-  }
+	while (!doneSelection) {
+		if (!bIfiRobotDisabled) doneSelection = true;
+		wait1Msec(25);
+	}
 
-  StopTask(selection);
-  bLCDBacklight = false;
+	StopTask(selection);
+	bLCDBacklight = false;
 }
